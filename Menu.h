03ab9af28b62bcee3c7cc573a3248153ad0d8d7f
James Spicer <spicerjames1182@hotmail.com>
2013-03-29 13:15:11 +0000

    starting to work on menu

--- Menu.h
+++ Menu.h
@@ -1,10 +1,26 @@
 #pragma once
+
+#include "Input.h"
+#include "d3dx9.h"
+
 class Menu
 {
 public:
-	Menu(void);
-	~Menu(void);
+	Menu();
+	~Menu();
 
-	bool Intialize();
+	bool Intialize(Input* input);
+	void update();
+	void render(LPDIRECT3DDEVICE9 device);
+	int getState();
+
+	enum{NEW_GAME = 1, EXIT = 2};
+
+private:
+
+	Input* _userInput;
+
+	int _menuItemSelected;
+	int _state;
 };
 
